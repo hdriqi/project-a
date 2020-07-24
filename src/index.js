502a382980329433013fb9220281f6618f2c8331
hdriqi <riqi.asdf@gmail.com>
2020-07-24 09:42:40 +0700

    add mongo driver

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cms from '../lib'
 
-cms()
+cms({
+  schemas: [
+    {
+      name: 'Post',
+      type: 'content',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          validation: (input) => {
+            return input.length > 0
+          },
+        }
+      ],
+      afterSave: (data) => {
+        console.log(`Sent email notification new post ${data.id} to subscribers`)
+      }
+    }
+  ]
+})
