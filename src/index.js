76b541631522295c21f688fde4e5e2966d0b30d5
hdriqi <riqi.asdf@gmail.com>
2020-07-24 19:30:23 +0700

    add sent mail

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from 'dotenv'
 import CMS from '../lib'
+import nodemailer from 'nodemailer'
+import aws from 'aws-sdk'
+
+const SENT_EMAIL = false
 
 dotenv.config()
 const cms = new CMS()
+
+const transporter = nodemailer.createTransport({
+  SES: new aws.SES({
+    apiVersion: '2010-12-01',
+    accessKeyId: process.env.IAM_USER_KEY,
+    secretAccessKey: process.env.IAM_USER_SECRET,
+    region: 'ap-southeast-1'
+  })
+})
 
 cms.registerComponents([
   {
@@ -33,6 +46,21 @@ cms.registerSchemas([
     ],
     afterSave: (data) => {
       console.log(`Sent email notification new post ${data.id} to subscribers`)
+      if (SENT_EMAIL) {
+        transporter.sendMail({
+          from: 'notification@evius.id',
+          to: 'riqi.asdf@gmail.com',
+          subject: 'New Data',
+          text: 'New Data on Post collection'
+        }, (err, info) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+          console.log(info.envelope);
+          console.log(info.messageId);
+        })
+      }
     }
   }
 ])
