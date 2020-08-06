87792e19357588a19aaf982811d67f92a663ea1f
hdriqi <riqi.asdf@gmail.com>
2020-08-06 12:36:13 +0700

    add schema data example

--- src/schemas.js
+++ src/schemas.js
@@ -1,17 +1,303 @@
-// fullname: router.query.fullname || '',
-//     class: router.query.class ||'',
-//     schoolName: '',
-//     stuPhoneNumber: '',
-//     stuEmail: '',
-//     parentTitle: '',
-//     parentFullname: '',
-//     parentPhoneNumber: '',
-//     parentEmail: '',
-//     addressProvince: '',
-//     addressCity: '',
-//     addressDetail: '',
-//     tutorNote: '',
-//     package: ''
+// const Student = {
+//   "fullname": "Hendri",
+//   "class": "12",
+//   "schoolName": "SMA Cinta Bersemi",
+//   "stuPhoneNumber": "0821321392",
+//   "stuEmail": "hendri@hanum.com",
+//   "parentTitle": "Bpk.",
+//   "parentFullname": "Surya",
+//   "parentPhoneNumber": "082183218321",
+//   "parentEmail": "surya@surya.com",
+//   "addressProvince": "Aceh",
+//   "addressCity": "Aceh Barat",
+//   "addressDetail": "Alamat lengkap cari gmaps",
+//   "tutorNote": "Yang cakep tutornya",
+//   "package": "48 Sesi",
+//   "additionalStudents": [
+//     "Hanum"
+//   ],
+//   "subjects": [
+//     "Matematika SMA",
+//     "Fisika SMA",
+//     "Kimia SMA"
+//   ],
+//   "schedules": [
+//     {
+//       "day": "Senin",
+//       "hour": "13.00"
+//     },
+//     {
+//       "day": "Senin",
+//       "hour": "15.00"
+//     },
+//     {
+//       "day": "Senin",
+//       "hour": "16.30"
+//     }
+//   ]
+// }
+
+// const Tutor = {
+//   "fullname": "Hendri Surya",
+//   "dateOfBirth": "Bangka, 32 Desember 1996",
+//   "addressProvince": "Bali",
+//   "addressCity": "Aceh Tenggara",
+//   "addressDetail": "Jalan Hendri No. 96",
+//   "transport": "Mobil",
+//   "phoneNumber": "08231832138128",
+//   "email": "hendri@surya.com",
+//   "eduUniversity": "Universitas Gajah Mada",
+//   "eduMajor": "ELINS",
+//   "eduGradYear": "2030",
+//   "eduGPA": "9.6",
+//   "eduExp": "Mengajar youtube",
+//   "eduAchievement": "Juara 1",
+//   "attachment": "",
+//   "schedules": [
+//     {
+//       "day": "Senin",
+//       "hour": "13.00"
+//     },
+//     {
+//       "day": "Selasa",
+//       "hour": "13.00"
+//     },
+//     {
+//       "day": "Rabu",
+//       "hour": "13.00"
+//     },
+//     {
+//       "day": "Kamis",
+//       "hour": "13.00"
+//     }
+//   ],
+//   "subjects": [
+//     "Matematika SMA",
+//     "Matematika SMP",
+//     "Matematika SD",
+//     "Calistung"
+//   ],
+//   "areas": [
+//     "Jakarta Selatan",
+//     "Depok"
+//   ]
+// }
+
+// const HomeData = {
+//   slides: [
+//     {
+//       img: `https://evius-dev.s3-ap-southeast-1.amazonaws.com/slide.png`,
+//       url: `/blog/123`
+//     },
+//     {
+//       img: `https://evius-dev.s3-ap-southeast-1.amazonaws.com/slide.png`,
+//       url: `/blog/123`
+//     }
+//   ],
+//   whyUs: [
+//     {
+//       icon: 'https://evius-dev.s3-ap-southeast-1.amazonaws.com/why-us-electrum-icon.svg',
+//       title: 'Laporan Progress',
+//       desc: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     },
+//     {
+//       icon: 'https://evius-dev.s3-ap-southeast-1.amazonaws.com/why-us-electrum-icon.svg',
+//       title: 'Laporan Progress',
+//       desc: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     },
+//     {
+//       icon: 'https://evius-dev.s3-ap-southeast-1.amazonaws.com/why-us-electrum-icon.svg',
+//       title: 'Laporan Progress',
+//       desc: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     },
+//     {
+//       icon: 'https://evius-dev.s3-ap-southeast-1.amazonaws.com/why-us-electrum-icon.svg',
+//       title: 'Laporan Progress',
+//       desc: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     }
+//   ],
+//   methods: [
+//     {
+//       name: 'Metode 1',
+//       img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`,
+//       desc: ``
+//     },
+//     {
+//       name: 'Metode 2',
+//       img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`
+//     }
+//   ],
+//   testimonials: [
+//     {
+//       name: 'John Doe',
+//       img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`,
+//       message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in mattis justo.`
+//     },
+//     {
+//       name: 'Bambang Suparno',
+//       img: `https://images.pexels.com/photos/1326947/pexels-photo-1326947.jpeg?cs=srgb&dl=pexels-giftpunditscom-1326947.jpg&fm=jpg`,
+//       message: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in mattis justo.`
+//     }
+//   ]
+// }
+
+// const faqsData = [
+//   {
+//     question: 'Bagaimana cara melakukan pendaftaran?',
+//     answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//   },
+//   {
+//     question: 'Apakah boleh memilih lebih dari satu jadwal?',
+//     answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//   },
+//   {
+//     question: 'Bagaimana cara menjadi tentor?',
+//     answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//   }
+// ]
+
+// const postsData = [
+//   {
+//     _id: 'asd',
+//     title: 'Bagaimana cara melakukan pendaftaran?',
+//     img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//     createdAt: new Date().getTime()
+//   },
+//   {
+//     _id: 'asd',
+//     title: 'Bagaimana cara melakukan pendaftaran?',
+//     img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//     createdAt: new Date().getTime()
+//   },
+//   {
+//     _id: 'asd',
+//     title: 'Bagaimana cara melakukan pendaftaran?',
+//     img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//     createdAt: new Date().getTime()
+//   },
+//   {
+//     _id: 'asd',
+//     title: 'Bagaimana cara melakukan pendaftaran?',
+//     img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//     createdAt: new Date().getTime()
+//   },
+//   {
+//     _id: 'asd',
+//     title: 'Bagaimana cara melakukan pendaftaran?',
+//     img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac quis auctor diam risus enim, fusce. Eros, cursus dignissim risus, risus pellentesque interdum placerat aliquet. Tellus eget sapien, in tempor sagittis. Id odio dictumst hac ut neque amet, adipiscing. Proin amet, nullam vulputate lectus tellus non ultricies vestibulum.',
+//     createdAt: new Date().getTime()
+//   }
+// ]
+
+// const aboutData = {
+//   intro: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ridiculus sed fermentum vulputate lacinia dictumst in dapibus. Duis turpis ac facilisis pellentesque porttitor a, amet. In cursus velit quisque vivamus placerat malesuada.',
+//   vision: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ridiculus sed fermentum vulputate lacinia dictumst in dapibus. Duis turpis ac facilisis pellentesque porttitor a, amet. In cursus velit quisque vivamus placerat malesuada.',
+//   mission: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ridiculus sed fermentum vulputate lacinia dictumst in dapibus. Duis turpis ac facilisis pellentesque porttitor a, amet. In cursus velit quisque vivamus placerat malesuada.',
+//   teams: [
+//     {
+//       img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//       name: 'Jane Doe',
+//       position: 'Chief Executive Officer',
+//       quotes: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     },
+//     {
+//       img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//       name: 'Jane Doe',
+//       position: 'Chief Executive Officer',
+//       quotes: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     },
+//     {
+//       img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//       name: 'Jane Doe',
+//       position: 'Chief Executive Officer',
+//       quotes: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     },
+//     {
+//       img: 'https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+//       name: 'Jane Doe',
+//       position: 'Chief Executive Officer',
+//       quotes: 'Perkembanganmu akan kami catat oleh tutor agar kamu bisa mengetahui strategi belajar yang terbaik untukmu!'
+//     }
+//   ]
+// }
+
+// const pricingData = [
+//   {
+//     level: 'SD',
+//     class: '1-6',
+//     session: 1,
+//     price: [
+//       120000,
+//       220000,
+//       300000,
+//       360000,
+//       425000,
+//       450000,
+//       490000
+//     ],
+//   },
+//   {
+//     level: 'SD',
+//     class: '1-6',
+//     session: 8,
+//     price: [
+//       1200000,
+//       2200000,
+//       3000000,
+//       3600000,
+//       4250000,
+//       4500000,
+//       4900000
+//     ]
+//   },
+//   {
+//     level: 'SMP',
+//     class: '7-8',
+//     session: 1,
+//     price: [
+//       120000,
+//       220000,
+//       300000,
+//       360000,
+//       425000,
+//       450000,
+//       490000
+//     ]
+//   },
+//   {
+//     level: 'SMP',
+//     class: '7-8',
+//     session: 8,
+//     price: [
+//       1200000,
+//       2200000,
+//       3000000,
+//       3600000,
+//       4250000,
+//       4500000,
+//       4900000
+//     ]
+//   },
+//   {
+//     level: 'SMP',
+//     class: '9',
+//     session: 1,
+//     price: [
+//       120000,
+//       220000,
+//       300000,
+//       360000,
+//       425000,
+//       450000,
+//       490000
+//     ]
+//   }
+// ]
 
 module.exports = [
   {
@@ -115,7 +401,7 @@ module.exports = [
         type: "array_schedule",
         component: "schedule",
         componentType: "student",
-        scheduleMin:3
+        scheduleMin: 3
       },
       {
         name: "scheduleAdditional",
@@ -165,7 +451,7 @@ module.exports = [
         type: "text",
         component: "text",
         validation: (input) => {
-          return [input.length > 0, "Title must be at least 1 characters"];
+          return [input.length > 0, "Title must be at least 1 characters"]
         },
       },
       {
@@ -173,11 +459,11 @@ module.exports = [
         type: "schedule",
         component: "schedule",
         componentType: "teacher",
-        scheduleMin:4,
+        scheduleMin: 4,
         validation: (input) => {
-          return [input.length > 0, "Title must be at least 1 characters"];
+          return [input.length > 0, "Title must be at least 1 characters"]
         },
       },
     ]
   },
-];
+]
