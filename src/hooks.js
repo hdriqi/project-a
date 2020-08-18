fad4365906fb8e8730b8cd568775fb1bdf57bc34
hdriqi <riqi.asdf@gmail.com>
2020-08-18 10:04:34 +0700

    update hooks

--- src/hooks.js
+++ src/hooks.js
@@ -1,21 +1,40 @@
 module.exports = [
-  // {
-  //   name: 'Student',
-  //   afterCreate: async (ctx, data) => {
-  //     console.log(`Sent email notification new student ${data.id} to subscribers`)
-  //     ctx.email.sendMail({
-  //       from: 'notification@evius.id',
-  //       to: 'riqi.asdf@gmail.com',
-  //       subject: 'New Data',
-  //       text: 'New Data on Student collection'
-  //     }, (err, info) => {
-  //       if (err) {
-  //         console.log(err)
-  //         return
-  //       }
-  //       console.log(info.envelope)
-  //       console.log(info.messageId)
-  //     })
-  //   },
-  // }
+  {
+    name: 'Student',
+    afterCreate: async (ctx, data) => {
+      console.log(`Sent email notification new student ${data._id} to subscribers`)
+      ctx.email.sendMail({
+        from: 'notification@evius.id',
+        to: process.env.NOTIF_RECEIVER,
+        subject: `[Electrum #${data._id}] New Student Data`,
+        html: `Received new student data. Visit <a href="https://cms.rumahbelajarelectrum.com" target="_blank">Dashboard</a>`
+      }, (err, info) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(info.envelope)
+        console.log(info.messageId)
+      })
+    },
+  },
+  {
+    name: 'Tutor',
+    afterCreate: async (ctx, data) => {
+      console.log(`Sent email notification new student ${data._id} to subscribers`)
+      ctx.email.sendMail({
+        from: 'notification@evius.id',
+        to: process.env.NOTIF_RECEIVER,
+        subject: `[Electrum #${data._id}] New Tutor Data`,
+        html: `Received new tutor data. Visit <a href="https://cms.rumahbelajarelectrum.com" target="_blank">Dashboard</a>`
+      }, (err, info) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(info.envelope)
+        console.log(info.messageId)
+      })
+    },
+  }
 ]
