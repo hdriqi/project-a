866193b9076be853d58ab0898992406eb7a248d6
hdriqi <riqi.asdf@gmail.com>
2020-09-05 18:55:40 +0700

    add hooks for online class

--- src/hooks.js
+++ src/hooks.js
@@ -36,5 +36,24 @@ module.exports = [
         console.log(info.messageId)
       })
     },
+  },
+  {
+    name: 'Online',
+    afterCreate: async (ctx, data) => {
+      console.log(`Sent email notification new online student ${data._id} to subscribers`)
+      ctx.email.sendMail({
+        from: 'notification@evius.id',
+        to: process.env.NOTIF_RECEIVER,
+        subject: `[Electrum #${data._id}] New Online Student Data`,
+        html: `Received new online student data. Visit <a href="https://cms.rumahbelajarelectrum.com" target="_blank">Dashboard</a>`
+      }, (err, info) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+        console.log(info.envelope)
+        console.log(info.messageId)
+      })
+    },
   }
 ]
